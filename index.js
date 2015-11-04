504509a8ed9e4a30cb51e36061888f6ad4d438bf
Mike Bostock <mbostock@gmail.com>
2015-11-04 13:24:07 -0800

    Concise exports.

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-import interpolate from "./src/interpolate";
-import interpolateArray from "./src/interpolateArray";
-import interpolateNumber from "./src/interpolateNumber";
-import interpolateObject from "./src/interpolateObject";
-import interpolateRound from "./src/interpolateRound";
-import interpolateString from "./src/interpolateString";
-import interpolateTransform from "./src/interpolateTransform";
-import interpolateZoom from "./src/interpolateZoom";
-import interpolators from "./src/interpolators";
-
-export {
-  interpolate,
-  interpolateArray,
-  interpolateNumber,
-  interpolateObject,
-  interpolateRound,
-  interpolateString,
-  interpolateTransform,
-  interpolateZoom,
-  interpolators
-};
+export {default as interpolate} from "./src/interpolate";
+export {default as interpolateArray} from "./src/interpolateArray";
+export {default as interpolateNumber} from "./src/interpolateNumber";
+export {default as interpolateObject} from "./src/interpolateObject";
+export {default as interpolateRound} from "./src/interpolateRound";
+export {default as interpolateString} from "./src/interpolateString";
+export {default as interpolateTransform} from "./src/interpolateTransform";
+export {default as interpolateZoom} from "./src/interpolateZoom";
+export {default as interpolators} from "./src/interpolators";
