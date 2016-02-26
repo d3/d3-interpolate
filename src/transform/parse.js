90621519a00fe30b95702a4ea727b38fd8a6cc65
Mike Bostock <mbostock@gmail.com>
2016-02-26 14:15:53 -0800

    Shorten slightly.

--- src/transform/parse.js
+++ src/transform/parse.js
@@ -9,8 +9,7 @@ export function parseCss(value) {
   if (value === "none") return identity;
   if (!cssNode) cssNode = document.createElement("DIV"), cssRoot = document.documentElement, cssView = document.defaultView;
   cssNode.style.transform = value;
-  cssRoot.appendChild(cssNode);
-  value = cssView.getComputedStyle(cssNode, null).getPropertyValue("transform");
+  value = cssView.getComputedStyle(cssRoot.appendChild(cssNode), null).getPropertyValue("transform");
   cssRoot.removeChild(cssNode);
   var m = value.slice(7, -1).split(",");
   return decompose(+m[0], +m[1], +m[2], +m[3], +m[4], +m[5]);
