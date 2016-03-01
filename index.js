98ab1c2eede23ab9ed91385d3242cc6ab524d807
Mike Bostock <mbostock@gmail.com>
2016-03-01 14:18:13 -0800

    Simpler version export.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export {version} from "./build/version";
 export {default as interpolate} from "./src/value";
 export {default as interpolateArray} from "./src/array";
 export {default as interpolateNumber} from "./src/number";
