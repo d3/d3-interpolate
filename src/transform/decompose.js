e2eae094fe0ff8bc9b6295ed438b14aa7edebb0a
Mike Bostock <mbostock@gmail.com>
2016-06-15 10:40:59 -0700

    Fix decomposition of scale(0) transform.

--- src/transform/decompose.js
+++ src/transform/decompose.js
@@ -1,4 +1,4 @@
-var rad2deg = 180 / Math.PI;
+var degrees = 180 / Math.PI;
 
 export var identity = {
   translateX: 0,
@@ -10,24 +10,16 @@ export var identity = {
 };
 
 export default function(a, b, c, d, e, f) {
-  if (a * d === b * c) return null;
-
-  var scaleX = Math.sqrt(a * a + b * b);
-  a /= scaleX, b /= scaleX;
-
-  var skewX = a * c + b * d;
-  c -= a * skewX, d -= b * skewX;
-
-  var scaleY = Math.sqrt(c * c + d * d);
-  c /= scaleY, d /= scaleY, skewX /= scaleY;
-
+  var scaleX, scaleY, skewX;
+  if (scaleX = Math.sqrt(a * a + b * b)) a /= scaleX, b /= scaleX;
+  if (skewX = a * c + b * d) c -= a * skewX, d -= b * skewX;
+  if (scaleY = Math.sqrt(c * c + d * d)) c /= scaleY, d /= scaleY, skewX /= scaleY;
   if (a * d < b * c) a = -a, b = -b, skewX = -skewX, scaleX = -scaleX;
-
   return {
     translateX: e,
     translateY: f,
-    rotate: Math.atan2(b, a) * rad2deg,
-    skewX: Math.atan(skewX) * rad2deg,
+    rotate: Math.atan2(b, a) * degrees,
+    skewX: Math.atan(skewX) * degrees,
     scaleX: scaleX,
     scaleY: scaleY
   };
