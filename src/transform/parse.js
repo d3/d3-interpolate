c3284ccf9d1fe6024fd57bdf3a024355b12c60e6
Mike Bostock <mbostock@gmail.com>
2016-06-28 13:57:24 -0700

    Fix #22 - handle null consolidated transform.

--- src/transform/parse.js
+++ src/transform/parse.js
@@ -11,13 +11,15 @@ export function parseCss(value) {
   cssNode.style.transform = value;
   value = cssView.getComputedStyle(cssRoot.appendChild(cssNode), null).getPropertyValue("transform");
   cssRoot.removeChild(cssNode);
-  var m = value.slice(7, -1).split(",");
-  return decompose(+m[0], +m[1], +m[2], +m[3], +m[4], +m[5]);
+  value = value.slice(7, -1).split(",");
+  return decompose(+value[0], +value[1], +value[2], +value[3], +value[4], +value[5]);
 }
 
 export function parseSvg(value) {
+  if (value == null) return identity;
   if (!svgNode) svgNode = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  svgNode.setAttribute("transform", value == null ? "" : value);
-  var m = svgNode.transform.baseVal.consolidate().matrix;
-  return decompose(m.a, m.b, m.c, m.d, m.e, m.f);
+  svgNode.setAttribute("transform", value);
+  if (!(value = svgNode.transform.baseVal.consolidate())) return identity;
+  value = value.matrix;
+  return decompose(value.a, value.b, value.c, value.d, value.e, value.f);
 }
