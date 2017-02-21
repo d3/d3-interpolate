9c23e135dfba09278ac592f2bbf3af907d55de6f
Mike Bostock <mbostock@gmail.com>
2017-02-21 09:20:21 -0800

    Fix extra paren.

--- src/value.js
+++ src/value.js
@@ -15,6 +15,6 @@ export default function(a, b) {
       : b instanceof color ? rgb
       : b instanceof Date ? date
       : Array.isArray(b) ? array
-      : !b.valueOf || isNaN(b)) ? object
+      : !b.valueOf || isNaN(b) ? object
       : number)(a, b);
 }
