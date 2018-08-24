19e47e16df018e946b802403d2139c798e0024ca
Mike Bostock <mbostock@gmail.com>
2018-08-24 08:04:18 -0700

    Fix tests.

--- test/hcl-test.js
+++ test/hcl-test.js
@@ -10,52 +10,52 @@ tape("interpolateHcl(a, b) converts a and b to HCL colors", function(test) {
 });
 
 tape("interpolateHcl(a, b) interpolates in HCL and returns an RGB string", function(test) {
-  test.equal(interpolate.interpolateHcl("steelblue", "#f00")(0.2), "rgb(105, 120, 201)");
-  test.equal(interpolate.interpolateHcl("rgba(70, 130, 180, 1)", "rgba(255, 0, 0, 0.2)")(0.2), "rgba(105, 120, 201, 0.84)");
+  test.equal(interpolate.interpolateHcl("steelblue", "#f00")(0.2), "rgb(106, 121, 206)");
+  test.equal(interpolate.interpolateHcl("rgba(70, 130, 180, 1)", "rgba(255, 0, 0, 0.2)")(0.2), "rgba(106, 121, 206, 0.84)");
   test.end();
 });
 
 tape("interpolateHcl(a, b) uses the shortest path when interpolating hue difference greater than 180°", function(test) {
   var i = interpolate.interpolateHcl(color.hcl(10, 50, 50), color.hcl(350, 50, 50));
-  test.equal(i(0.0), "rgb(196, 79, 106)");
-  test.equal(i(0.2), "rgb(196, 79, 112)");
-  test.equal(i(0.4), "rgb(195, 79, 118)");
-  test.equal(i(0.6), "rgb(193, 79, 124)");
-  test.equal(i(0.8), "rgb(191, 80, 129)");
-  test.equal(i(1.0), "rgb(189, 81, 135)");
+  test.equal(i(0.0), "rgb(194, 78, 107)");
+  test.equal(i(0.2), "rgb(194, 78, 113)");
+  test.equal(i(0.4), "rgb(193, 78, 118)");
+  test.equal(i(0.6), "rgb(192, 78, 124)");
+  test.equal(i(0.8), "rgb(191, 78, 130)");
+  test.equal(i(1.0), "rgb(189, 79, 136)");
   test.end();
 });
 
 tape("interpolateHcl(a, b) uses the shortest path when interpolating hue difference greater than 360°", function(test) {
   var i = interpolate.interpolateHcl(color.hcl(10, 50, 50), color.hcl(380, 50, 50));
-  test.equal(i(0.0), "rgb(196, 79, 106)");
-  test.equal(i(0.2), "rgb(196, 79, 104)");
-  test.equal(i(0.4), "rgb(197, 80, 101)");
-  test.equal(i(0.6), "rgb(196, 80, 98)");
-  test.equal(i(0.8), "rgb(196, 80, 95)");
-  test.equal(i(1.0), "rgb(196, 81, 92)");
+  test.equal(i(0.0), "rgb(194, 78, 107)");
+  test.equal(i(0.2), "rgb(194, 78, 104)");
+  test.equal(i(0.4), "rgb(194, 79, 101)");
+  test.equal(i(0.6), "rgb(194, 79, 98)");
+  test.equal(i(0.8), "rgb(194, 80, 96)");
+  test.equal(i(1.0), "rgb(194, 80, 93)");
   test.end();
 });
 
 tape("interpolateHcl(a, b) uses the shortest path when interpolating hue difference greater than 540°", function(test) {
   var i = interpolate.interpolateHcl(color.hcl(10, 50, 50), color.hcl(710, 50, 50));
-  test.equal(i(0.0), "rgb(196, 79, 106)");
-  test.equal(i(0.2), "rgb(196, 79, 112)");
-  test.equal(i(0.4), "rgb(195, 79, 118)");
-  test.equal(i(0.6), "rgb(193, 79, 124)");
-  test.equal(i(0.8), "rgb(191, 80, 129)");
-  test.equal(i(1.0), "rgb(189, 81, 135)");
+  test.equal(i(0.0), "rgb(194, 78, 107)");
+  test.equal(i(0.2), "rgb(194, 78, 113)");
+  test.equal(i(0.4), "rgb(193, 78, 118)");
+  test.equal(i(0.6), "rgb(192, 78, 124)");
+  test.equal(i(0.8), "rgb(191, 78, 130)");
+  test.equal(i(1.0), "rgb(189, 79, 136)");
   test.end();
 });
 
 tape("interpolateHcl(a, b) uses the shortest path when interpolating hue difference greater than 720°", function(test) {
   var i = interpolate.interpolateHcl(color.hcl(10, 50, 50), color.hcl(740, 50, 50));
-  test.equal(i(0.0), "rgb(196, 79, 106)");
-  test.equal(i(0.2), "rgb(196, 79, 104)");
-  test.equal(i(0.4), "rgb(197, 80, 101)");
-  test.equal(i(0.6), "rgb(196, 80, 98)");
-  test.equal(i(0.8), "rgb(196, 80, 95)");
-  test.equal(i(1.0), "rgb(196, 81, 92)");
+  test.equal(i(0.0), "rgb(194, 78, 107)");
+  test.equal(i(0.2), "rgb(194, 78, 104)");
+  test.equal(i(0.4), "rgb(194, 79, 101)");
+  test.equal(i(0.6), "rgb(194, 79, 98)");
+  test.equal(i(0.8), "rgb(194, 80, 96)");
+  test.equal(i(1.0), "rgb(194, 80, 93)");
   test.end();
 });
 
@@ -84,11 +84,11 @@ tape("interpolateHcl(a, b) uses b’s chroma when a’s chroma is undefined", fu
 });
 
 tape("interpolateHcl(a, b) uses b’s luminance when a’s luminance is undefined", function(test) {
-  test.equal(interpolate.interpolateHcl(null, color.hcl(20, 80, 50))(0.5), "rgb(234, 19, 77)");
+  test.equal(interpolate.interpolateHcl(null, color.hcl(20, 80, 50))(0.5), "rgb(230, 13, 79)");
   test.end();
 });
 
 tape("interpolateHcl(a, b) uses a’s luminance when b’s luminance is undefined", function(test) {
-  test.equal(interpolate.interpolateHcl(color.hcl(20, 80, 50), null)(0.5), "rgb(234, 19, 77)");
+  test.equal(interpolate.interpolateHcl(color.hcl(20, 80, 50), null)(0.5), "rgb(230, 13, 79)");
   test.end();
 });
--- test/lab-test.js
+++ test/lab-test.js
@@ -10,8 +10,8 @@ tape("interpolateLab(a, b) converts a and b to Lab colors", function(test) {
 });
 
 tape("interpolateLab(a, b) interpolates in Lab and returns an RGB string", function(test) {
-  test.equal(interpolate.interpolateLab("steelblue", "#f00")(0.2), "rgb(138, 119, 147)");
-  test.equal(interpolate.interpolateLab("rgba(70, 130, 180, 1)", "rgba(255, 0, 0, 0.2)")(0.2), "rgba(138, 119, 147, 0.84)");
+  test.equal(interpolate.interpolateLab("steelblue", "#f00")(0.2), "rgb(134, 120, 146)");
+  test.equal(interpolate.interpolateLab("rgba(70, 130, 180, 1)", "rgba(255, 0, 0, 0.2)")(0.2), "rgba(134, 120, 146, 0.84)");
   test.end();
 });
 
