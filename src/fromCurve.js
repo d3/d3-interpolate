c46d8599bdfcc471ea453abae608fec132cdd79e
James Saunders <james@saunders-family.net>
2019-07-14 20:27:42 +0100

    Small tweaks following PR comments from Andreas Plesch.

--- src/fromCurve.js
+++ src/fromCurve.js
@@ -36,6 +36,7 @@ function svgPathInterpolator(path, epsilon, samples) {
       let iter = 0;
 
       while (Math.abs(delta) > epsilon && iter < samples) {
+        if (iter > samples) return false;
         iter++;
 
         if (reverse * delta < 0) {
@@ -46,9 +47,7 @@ function svgPathInterpolator(path, epsilon, samples) {
           l = (mn + l) / 2;
         }
         nextDelta = svgpath.getPointAtLength(l).x - targetX;
-        if (Math.abs(Math.abs(delta) - Math.abs(nextDelta)) < epsilon) {
-          break;
-        }
+
         delta = nextDelta;
       }
 
