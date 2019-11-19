24d1dea456996cf94860cdc16bb7ff898aaa30de
Mike Bostock <mbostock@gmail.com>
2019-11-19 14:40:43 -0800

    Update numberArray.js

--- src/numberArray.js
+++ src/numberArray.js
@@ -1,10 +1,10 @@
 export default function(a, b) {
   if (!b) b = [];
-  var na = a ? Math.min(b.length, a.length) : 0,
-    c = b.slice(),
-    i;
+  var n = a ? Math.min(b.length, a.length) : 0,
+      c = b.slice(),
+      i;
   return function(t) {
-    for (i = 0; i < na; ++i) c[i] = a[i] * (1 - t) + b[i] * t;
+    for (i = 0; i < n; ++i) c[i] = a[i] * (1 - t) + b[i] * t;
     return c;
   };
 }
